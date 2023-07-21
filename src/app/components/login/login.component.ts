7828b742925bafb6b3615ec924b58c637b0cc750
jomaosssss <joma.ostgo@gmail.com>
2023-07-21 09:47:44 -0600

    login modal

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,11 +7,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
-
+  constructor(private router: Router) { }
+  
   email: string = '';
   password: string = '';
   fullname: string = '';
+
+  email2: string = '';
+  password2: string = '';
+  fullname2: string = '';
 
   login() {
     // alert(`email: ${this.email}, password: ${this.password}`);
@@ -19,32 +23,43 @@ export class LoginComponent {
     const user: object = {
       email: this.email,
       password: this.password,
-      fullname: this.fullname,
     };
 
     if (this.validar()) {
       sessionStorage.setItem('email', this.email);
       // sessionStorage.setItem('password', this.password);
-      sessionStorage.setItem('fullname', this.fullname);
+
+
+      this.router.navigate(['/home']);
+    }
+  }
+
+  signup() {
+    // alert(`email: ${this.email}, password: ${this.password}`);
+    // this.validar();
+    const user: object = {
+      email2: this.email2,
+      password2: this.password2,
+      fullname2: this.fullname2,
+    };
+
+    if (this.validar2()) {
+      sessionStorage.setItem('email2', this.email2);
+      // sessionStorage.setItem('password', this.password);
+      sessionStorage.setItem('fullname2', this.fullname2);
 
       this.router.navigate(['/home']);
     }
   }
 
   validar() {
-    if (!this.fullname || !this.password || !this.email) {
+    if (!this.password || !this.email) {
       alert('Todos los campos son obligatorios');
-      return false;
-    }
-    // valida nombre
-    const fullnameRegex = /^[a-zA-Z\s]+$/;
-    if (!fullnameRegex.test(this.fullname)) {
-      alert('El nombre solo puede contener letras y numeros');
       return false;
     }
 
     // validar password
-    if (this.password.length < 6) {
+    if (this.password.length < 8) {
       alert('la constraseña debe contener al menos 6 caracteres');
       return false;
     }
@@ -52,6 +67,33 @@ export class LoginComponent {
     // validar email
     const emailregex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailregex.test(this.email)) {
+      alert('Correo electrónico inválido.');
+      return false;
+    }
+    return true;
+  }
+
+  validar2() {
+    if (!this.fullname2 || !this.password2 || !this.email2) {
+      alert('Todos los campos son obligatorios2');
+      return false;
+    }
+    // valida nombre
+    const fullname2Regex = /^[a-zA-Z\s]+$/;
+    if (!fullname2Regex.test(this.fullname2)) {
+      alert('El nombre solo puede contener letras y numeros');
+      return false;
+    }
+
+    // validar password
+    if (this.password2.length < 8) {
+      alert('la constraseña debe contener al menos 6 caracteres');
+      return false;
+    }
+
+    // validar email
+    const email2regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email2regex.test(this.email2)) {
       alert('Correo electrónico inválido.');
       return false;
     }
